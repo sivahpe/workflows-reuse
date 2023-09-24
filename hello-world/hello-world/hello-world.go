0f740f56071e6dae1417aa6db07bd0fad239b2e1
Siva Sankar Gali <shiva.shankar@hpe.com>
2023-09-24 20:02:32 +0100

    Adding app

---
+++ hello-world/hello-world/hello-world.go
@@ -0,0 +1,13 @@
+// Copyright 2022 Hewlett Packard Enterprise Development LP
+
+package main
+
+import "fmt"
+
+func Hello() string {
+	return "hello world"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
